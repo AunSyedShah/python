204974136a11079946d88615dc6173134995521b
Syed Aun Muhammad <48954645+AunSyedShah@users.noreply.github.com>
2020-06-29 21:25:40 +0500

    Update matrix.py

--- math/matrix.py
+++ math/matrix.py
@@ -1,11 +1,14 @@
+# matrix in 2 dimensional 2 x 2 matrix
 m = [
     [2, 3],
     [5, 6]
 ]
+# adjoint left blank for future use
 adjoint = [
     [ 0, 0],
     [0, 0]
-] 
+]
+# calculating determinant
 d = (m[0][0] * m[1][1]) - (m[0][1] * m[1][0])
 if d != 0:
     adjoint[0][0] = m[1][1]
@@ -14,3 +17,5 @@ if d != 0:
     adjoint[1][1] = m[0][0]
     for i in adjoint:
         print(i)
+else:
+    print("matrix is non singular")
